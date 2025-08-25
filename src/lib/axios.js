2856c608747aebed87e669155bcbd85ab0392d88
Riaz115 <riaz90603@gmail.com>
2025-08-25 09:47:54 +0500

    front end fixing

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,9 +2,9 @@ import axios from "axios"
 
 
 const axiosInstance = axios.create({
-    // baseURL : "http://localhost:5000/api/v1",
+    baseURL : "https://zmyceexy5p.ap-south-1.awsapprunner.com/api/v1",
     // baseURL : "http://ec2-13-245-5-146.af-south-1.compute.amazonaws.com:5000/api/v1",
-    baseURL : "http://localhost:4000/api/v1",
+    // baseURL : "http://localhost:4000/api/v1",
     withCredentials: true,
     // headers: {
     //     "Content-Type": "application/json"
