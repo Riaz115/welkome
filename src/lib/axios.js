67b3c37e6ae7a0b4310c212495e8afd85005f620
Riaz115 <riaz90603@gmail.com>
2025-08-26 17:22:18 +0500

    add product

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,9 +2,12 @@ import axios from "axios"
 
 
 const axiosInstance = axios.create({
-    // baseURL : "https://zmyceexy5p.ap-south-1.awsapprunner.com/api/v1",
+
+    //this is mjy new url
+    baseURL : "https://zmyceexy5p.ap-south-1.awsapprunner.com/api/v1",
+
     // baseURL : "http://ec2-13-245-5-146.af-south-1.compute.amazonaws.com:5000/api/v1",
-    baseURL : "http://localhost:4000/api/v1",
+    // baseURL : "http://localhost:4000/api/v1",
     withCredentials: true,
     // headers: {
     //     "Content-Type": "application/json"
