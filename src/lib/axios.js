b74664325e1ae48c01ccf8f8267a8da4bd21daf2
Riaz115 <riaz90603@gmail.com>
2025-08-27 12:40:56 +0500

    solving cors errors

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from "axios"
 const axiosInstance = axios.create({
 
     //this is mjy new url
-    baseURL : "http://monkmaze-alb-1433160481.ap-south-1.elb.amazonaws.com",
+    baseURL : "http://monkmaze-alb-1433160481.ap-south-1.elb.amazonaws.com/api/v1",
 
     // baseURL : "http://ec2-13-245-5-146.af-south-1.compute.amazonaws.com:5000/api/v1",
     // baseURL : "http://localhost:4000/api/v1",
